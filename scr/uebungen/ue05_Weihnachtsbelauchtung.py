2baf1d6887f1b9d393ba38a1c0e8f4f8f5f04fba
PrattnerErik <praerz21@htl-kaindorf.at>
2025-02-03 09:59:52 +0100

    aktueller stand LED STRIP

--- scr/uebungen/ue05_Weihnachtsbelauchtung.py
+++ scr/uebungen/ue05_Weihnachtsbelauchtung.py
@@ -9,12 +9,11 @@ from rpi_ws281x import ws, Adafruit_NeoPixel, Color
 import argparse
 
 # LED strip configuration:
-LED_COUNT = 16        # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_COUNT = 3        # Number of LED pixels.
+LED_PIN = 21          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP = ws.WS2811_STRIP_GRB
