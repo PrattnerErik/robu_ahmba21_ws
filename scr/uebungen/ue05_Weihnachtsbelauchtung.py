68d65363dee50ff5ecb1138aaff015b5537c2835
PrattnerErik <praerz21@htl-kaindorf.at>
2025-02-03 10:32:01 +0100

    stunde 3.2.25

--- scr/uebungen/ue05_Weihnachtsbelauchtung.py
+++ scr/uebungen/ue05_Weihnachtsbelauchtung.py
@@ -1,7 +1,7 @@
 
-https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf
-https://pypi.org/project/rpi-ws281x/
-https://github.com/rpi-ws281x/rpi-ws281x-python/
+# https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf
+# https://pypi.org/project/rpi-ws281x/
+# https://github.com/rpi-ws281x/rpi-ws281x-python/
 
 
 import time
@@ -11,9 +11,23 @@ import argparse
 # LED strip configuration:
 LED_COUNT = 3        # Number of LED pixels.
 LED_PIN = 21          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_INVERT = False
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP = ws.WS2811_STRIP_GRB
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT,
+                          LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+
+strip.begin()
+
+colors = [Color(255,0,0),Color(255,255,0),Color(0,255,0)]
+
+while True:
+    for i in range(LED_COUNT):#erzeugt werte 0,1,2, für i wenn LED_COUNT == 3
+        strip.setPixelColor(0,colors[i])#stellt farbe an led ein
+        strip.setPixelColor(1,colors[i])
+        strip.setPixelColor(2,colors[i])
+    strip.show()
